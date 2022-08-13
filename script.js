83cc2e067782fc21e95549f3773c40f0b96821c1
inan yigit <inanygt@hotmail.com>
2022-08-13 15:22:48 +0200

    added hover in js

--- script.js
+++ script.js
@@ -2,18 +2,37 @@
 
 let container = document.querySelector(".container")
 
-// let grid1 = document.createElement("div")
-// grid1.setAttribute("class", "grid")
-// container.append(grid1)
-
 function makeRows(rows,cols) {
-   // container.style.setProperty('--grid-rows', rows);
-   // container.style.setProperty('--grid-cols', cols);
    for (let i = 0; i < (rows * cols); i++) {
       let cell = document.createElement("div");
-      // cell.innerText = (i + 1);
       container.appendChild(cell).className = "grid-item";
    }
 }
 
 makeRows(16,16);
+
+// HOVER
+
+let gridMouse = document.querySelectorAll(".grid-item")
+
+console.log(gridMouse)
+
+// gridMouse.addEventListener("mouseover", function() {
+//    gridMouse.setAttribute("style", "background-color:blue;");
+// })
+
+// gridMouse.addEventListener("mouseout", function() {
+//    gridMouse.setAttribute("style", "background-color: white;")
+// })
+
+gridMouse.forEach(grid => {
+   grid.addEventListener("mouseover", function() {
+      grid.style.backgroundColor = "black";
+   })
+})
+
+gridMouse.forEach(grid => {
+   grid.addEventListener("mouseout", function() {
+      grid.style.backgroundColor = "white";
+   })
+})
